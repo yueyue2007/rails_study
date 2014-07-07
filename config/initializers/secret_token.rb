0b7053e22c8097922a085c8a559ae91751e81cab
saasbook <saasbook@saasbook.(none)>
2014-07-06 19:41:14 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '79f74175ede4af011b51d3d64d66e7e4f8dce836ef49c580f1a899c4c34398705a76cf993ccc584cc7253e641e0c423823c61ac766e50509dfe4c20de7d72bf1'
